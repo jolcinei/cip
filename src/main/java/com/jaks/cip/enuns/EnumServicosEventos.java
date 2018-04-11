6ce982688a13342dbffd7e32082113e835722f27
jolcinei <jaks.gomes@gmail.com>
2018-04-10 21:54:00 -0300

    Adicionados Tipos de Serviços conforme Manual de Processamento SLC

--- src/main/java/com/jaks/cip/enuns/EnumServicosEventos.java
+++ src/main/java/com/jaks/cip/enuns/EnumServicosEventos.java
@@ -13,17 +13,25 @@ import java.io.Serializable;
  */
 public enum EnumServicosEventos implements Serializable {
 
-    //Credenciador
+    //Credenciador para CIP
     ASLC027("027", "Credenciador informa as liquidações das transações de cartões de crédito."),
     ASLC029("029", "Credenciador informa as liquidações das transações de cartões de débito."),
     ASLC031("031", "Credenciador informa as liquidações das transações de antecipações de recebíveis de cartões de pagamento."),
-    //Domicilio
-    //ASLC022("022", "SLC informa as liquidações das transações de cartões de crédito para IF Domicilio."),
+    //CIP para Credenciador
+    ASLC027RET("027RET", "Retorno do processamento das informacoes com as liquidacoes das transacoes de cartoes de credito."),
+    ASLC029RET("029RET", "Retorno do processamento das informacoes com as liquidacoes das transacoes de cartoes de debito."),
+    ASLC031RET("031RET", "Retorno do processamento das liquidacoes das transacoes de antecipações de recebíveis de cartões de pagamento."),
+    ASLC028("028", "SLC informa o retorno do processamento das liquidacoes das transacoes de cartoes de credito."),
+    ASLC030("030", "SLC informa o retorno do processamento das liquidacoes das transacoes de cartoes de debito."),
+    ASLC034("034", "SLC informa o retorno do processamento das liquidacoes das transacoes de antecipações de recebíveis de cartões de pagamento."),
+    //Domicilio para a CIP
     ASLC023("023", "IF Domicilio informa o retorno do processamento das informacoes com liquidacoes das transacoes de cartoes de CREDITO."),
-    //ASLC024("024", "SLC informa as liquidações das transações de cartões de debito para IF Domicilio."),
     ASLC025("025", "IF Domicilio informa o retorno do processamento das informacoes com liquidacoes das transacoes de cartoes de DEBITO."),
-    //ASLC032("022", "SLC informa as liquidações das transações de antecipacoes de recebiveis de cartoes de pagamento para IF Domicilio."),
-    ASLC033("022", "IF Domicilio informa o retorno do processamento das informacoes com liquidacoes das antecipacoes de recebiveis de cartoes de pagamento.");
+    ASLC033("022", "IF Domicilio informa o retorno do processamento das informacoes com liquidacoes das antecipacoes de recebiveis de cartoes de pagamento."),
+    //CIP para Domicilio    
+    ASLC022("022", "SLC informa as liquidações das transações de cartões de crédito para IF Domicilio."),
+    ASLC024("024", "SLC informa as liquidações das transações de cartões de debito para IF Domicilio."),
+    ASLC032("022", "SLC informa as liquidações das transações de antecipacoes de recebiveis de cartoes de pagamento para IF Domicilio.");
 
     private String codigo;
 
